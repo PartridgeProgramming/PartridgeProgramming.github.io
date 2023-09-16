85ece3c4aeeb71e14b455b2f9becd186afa45b7a
Alden <aldenp@email.sc.edu>
2023-09-16 14:15:30 -0400

    Assignment 6

--- assignments/Assignment 6/script.js
+++ assignments/Assignment 6/script.js
@@ -6,13 +6,41 @@ const hideImage = () => {
     document.getElementById("image").classList.add("hide");
 }
 
-const addComment = () => {
-    document.getElementsByClassName("comment").value="";
+const commenting = () => {
+    const addComment = document.getElementById("comments");
+
+    // Creating elements
+    const commentPara = document.createElement("p");
+    const commentH3 = document.createElement("h3");
+    
+    // Set heading for comment
+    commentH3.appendChild(document.createTextNode(document.getElementById("product-name-txt").value));
+    
+    // Body and formatting of comment
+    commentPara.appendChild(document.createTextNode(document.getElementById("rating-txt").value + "/5 stars "));
+    commentPara.appendChild(document.createTextNode(document.getElementById("comment-txt").value));
+    commentPara.appendChild(document.createElement("br")); // Add another line break
+    commentPara.appendChild(document.createTextNode("by " + document.getElementById("user-name-txt").value));
+
+    // Adding in the formatted comment
+    addComment.appendChild(commentH3);
+    addComment.appendChild(commentPara);
+
+    // Clear input fields
+    document.getElementById("product-name-txt").value = "";
+    document.getElementById("comment-txt").value = "";
+    document.getElementById("rating-txt").value = "";
+    document.getElementById("user-name-txt").value = "";
+}
+
+const makeDance = () => {
+    document.getElementById("heart").classList.add("dancing");
 }
 
 window.onload = () => {
     //get button, tie function to clickage
     document.getElementById("button-show").onclick = showImage;
     document.getElementById("button-hide").onclick = hideImage;
-    document.getElementById("button-comment").onclick = addComment;
+    document.getElementById("button-dance").onclick = makeDance;
+    document.getElementById("button-comment").onclick = commenting;
 }
--- class/sept14/script.js
+++ class/sept14/script.js
@@ -1,1 +1,45 @@
 /* Template JavaScript file */
+//const add = (a, b) => { return a+b };
+
+//console.log(add(5, 6));
+
+//const square = a => a * a;
+//const square = (a) => a * a;
+
+//console.log(square(5));
+
+//const hello = () => console.log("Hello me!");
+
+//hello();
+
+//const helloSpecific = userName => console.log("Hello " + userName + "!");
+
+//helloSpecific("Portia");
+/*
+const helloFullName = (firstName, lastName) => {
+    console.log("Hello " + firstName + " " + lastName);
+    console.log("You are awesome!");
+};
+
+helloFullName("portia", "plante");
+*/
+/* Not ok to change a constant
+const myName = "Portia";
+myName = "Sally";
+*/
+
+//const myFunct = () => console.log("Hey you!");
+
+const startAnimatation = () => {
+    document.getElementById("square").classList.add("animated");
+}
+
+const displayName = () => {
+    const firstName = document.getElementById("txt-first-name").value;
+    console.log("Hello " + firstName + "!");
+}
+
+window.onload = () => {
+    document.getElementById("button-begin").onclick = startAnimatation;
+    document.getElementById("button-show-name").onclick = displayName;
+}
