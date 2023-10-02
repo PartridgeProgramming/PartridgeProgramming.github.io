6dddf8a4467d6cfbbebc28cdb044bcabb63660b0
Alden <aldenp@email.sc.edu>
2023-10-01 23:40:31 -0400

    Current work

---
+++ script.js
@@ -0,0 +1,17 @@
+const toggleAssignments = () => {
+    document.getElementById("assignments-view").classList.toggle("hidden");
+    if (!document.getElementById("project-view").classList.contains("hidden")) {
+        document.getElementById("project-view").classList.toggle("hidden");
+    }
+};
+
+const toggleProject = () => {
+    document.getElementById("project-view").classList.toggle("hidden");
+    if (!document.getElementById("assignments-view").classList.contains("hidden")) {
+        document.getElementById("assignments-view").classList.toggle("hidden");
+    }
+};
+window.onload = () => {
+    document.getElementById("assignment-link").onclick = toggleExercise1;
+    document.getElementById("project-link").onclick = toggleExercise2;
+};
